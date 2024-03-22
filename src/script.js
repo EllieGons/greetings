2d6069ada191ee6081689791797f58a9044a3dac
EllieGons <72343072+EllieGons@users.noreply.github.com>
2024-03-22 17:28:43 -0300

    Update script.js

--- src/script.js
+++ src/script.js
@@ -3,19 +3,16 @@ function toggleDropdown() {
     colorOptions.style.display = (colorOptions.style.display === "block") ? "none" : "block";
 }
 
-
-
 function selectColor(color) {
     var selectedColor = document.querySelector(".selected-color .color-preview");
     var selectedColorName = document.querySelector(".selected-color span");
     selectedColor.style.backgroundColor = color;
-    selectedColorName.textContent = getColorName(color); // Set color name
-    updateGreetingCardAndLink(); // Update greeting card color instantly
-    toggleDropdown(); // Hide the dropdown after selecting a color
+    selectedColorName.textContent = getColorName(color);
+    updateGreetingCardAndLink();
+    toggleDropdown();
 }
 
 function getColorName(color) {
-    // Map colors to their corresponding names
     var colorNames = {
         "#B89F89": "Brown",
         "#B6B498": "Green",
@@ -27,42 +24,44 @@ function getColorName(color) {
 
 
 
-// REST Function to update the greeting card and link
+
 function updateGreetingCardAndLink() {
-    var userName = document.getElementById('user-name').value || 'Muffin'; // Get user name value
+    var userName = document.getElementById('user-name').value || 'Muffin'; 
     var color = document.querySelector(".selected-color .color-preview").style.backgroundColor;
     var link = generateUniqueLink(userName, color);
 
-    // Update greeting, date, and link elements
+    
     document.getElementById('greeting').textContent = getGreeting(new Date().getHours()) + ', ' + userName + '!';
     document.getElementById('date').textContent = getDateInfo(new Date());
     document.getElementById('generated-link').value = link;
 
-    // Update greeting card styles
-    document.getElementById('greeting-card').style.color = color; // Update text color
-    document.getElementById('greeting-card').style.borderColor = color; // Update border color
+    
+    document.getElementById('greeting-card').style.color = color;
+    document.getElementById('greeting-card').style.borderColor = color;
 }
 
 
-// Function to get the current greeting
+
 function getGreeting(hour) {
     if (hour >= 5 && hour < 12) {
         return 'morning';
     } else if (hour >= 12 && hour < 18) {
         return 'afternoon';
-    } else {
+    } else if (hour >= 18 && hour < 22) {
         return 'evening';
+    } else {
+        return 'night';
     }
 }
 
-// Function to get the current date information
+
 function getDateInfo(date) {
     var dayOfWeek = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
     var monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
     return dayOfWeek[date.getDay()] + ', ' + date.getDate() + ' ' + monthNames[date.getMonth()] + ' ' + date.getFullYear();
 }
 
-// Function to generate a unique link with user input
+
 function generateUniqueLink(userName, color) {
     var defaultLink = "https://elliegons.github.io/greetings/widget/";
     var query = "user=" + encodeURIComponent(userName) + "&color=" + encodeURIComponent(color);
@@ -71,7 +70,7 @@ function generateUniqueLink(userName, color) {
 
 
 
-// Function to copy the generated link to clipboard
+
 function copyLink() {
     var linkInput = document.getElementById('generated-link');
     linkInput.select();
@@ -81,26 +80,24 @@ function copyLink() {
     var buttonText = copyLinkBtn.querySelector('.button-text');
     var icon = copyLinkBtn.querySelector('i');
 
-    // Change the text of the button
+    
     buttonText.textContent = 'Copied!';
     
-    // Hide the icon
+    
     icon.style.display = 'none';
 
     setTimeout(function() {
-        // Revert back to original text after 2 seconds
         buttonText.textContent = 'Copy Link';
-        // Show the icon
         icon.style.display = 'inline-block';
     }, 3000);
 }
 
 
-// Add event listener to copy link button
+
 document.getElementById('copy-link-btn').addEventListener('click', copyLink);
 
-// Add event listener for input change to update greeting card and link
+
 document.getElementById('user-name').addEventListener('input', updateGreetingCardAndLink);
 
-// Initial update of greeting card and link
+
 updateGreetingCardAndLink();
